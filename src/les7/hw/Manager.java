903799721e3920c85ffbbf6781077def682f4336
zhivodir <dzhivchik@gmail.com>
2016-11-02 22:09:33 +0200

    hw7 task1: right????

--- src/les7/hw/Manager.java
+++ src/les7/hw/Manager.java
@@ -14,23 +14,14 @@ public class Manager {
     }
 
     public void tryTakeDock(Ship ship) {
-        int count = 0;
         for(int i = 0; i < docks.length; i++){
             if(!docks[i].isBusy()){
-                docks[i].setBusy();
-                docks[i].unloadOneBox(ship);
-                System.out.println("Dock " + (i+1) + " unloaded box from " + ship.getName() +
-                        ". Aboard: " + ship.getCountOfBoxes());
-                notifyAll();
-            }else{
-                count++;
-            }
-        }
-        if(count == 2){
-            try{
-                wait();
-            }catch(InterruptedException e){
-                System.out.println(e);
+                synchronized (docks[i]) {
+                    docks[i].setBusy();
+                    docks[i].unloadOneBox(ship);
+                    System.out.println("Dock " + (i + 1) + " unloaded box from " + ship.getName() +
+                            ". Aboard: " + ship.getCountOfBoxes());
+                }
             }
         }
     }
