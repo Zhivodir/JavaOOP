6173b2989806161dcae1b38f81f250fa0994f056
zhivodir <dzhivchik@gmail.com>
2016-11-14 22:43:23 +0200

    hw10 task4load alphabet from file

--- src/les10/hw/task4/ASCIIAlphabet.java
+++ src/les10/hw/task4/ASCIIAlphabet.java
@@ -1,8 +1,6 @@
 package les10.hw.task4;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,22 +10,42 @@ import java.util.Map;
  * Created by User on 11.11.2016.
  */
 public class ASCIIAlphabet {
-    Map<Character, String[]> alphabet = new HashMap<>();
-    List<String[]> onPrint = new ArrayList<>();
+    private File file;
+    private Map<Character, String[]> alphabet = new HashMap<>();
+    private List<String[]> onPrint = new ArrayList<>();
 
-    public ASCIIAlphabet() {
-        alphabet.put('A', new String[]{"  **   "," *  *  ","*    * ","****** ","*    * ","*    * "});
-        alphabet.put('B', new String[]{"*****  ","*    * ","*****  ","*****  ","*    * ","*****  "});
-        alphabet.put('C', new String[]{"  **** "," *     ","*      ","*      "," *     ","  **** "});
-        alphabet.put('E', new String[]{"****** ","*      ","****   ","*      ","*      ","****** "});
-        alphabet.put('L', new String[]{"*      ","*      ","*      ","*      ","*      ","****** "});
-        alphabet.put('M', new String[]{"*    * ","**  ** ","* ** * ","*    * ","*    * ","*    * "});
-        alphabet.put('P', new String[]{"*****  ","*    * ","*    * ","*****  ","*      ","*      "});
-        alphabet.put('X', new String[]{"*    * "," *  *  ","  **   ","  **   "," *  *  ","*    * "});
-        alphabet.put('.', new String[]{"       ","       ","       ","       ","**     ","**     "});
+    public ASCIIAlphabet(File file) {
+        this.file = file;
+        loadAlphabetFromFile();
         alphabet.put('\u0020', new String[]{"       ","       ","       ","       ","       ","       "});
         alphabet.put('\u0009', new String[]{"       ","       ","       ","       ","       ","       "});
+        //printListTest();
+    }
 
+    public void loadAlphabetFromFile(){
+        String text = "";
+        Character sign = null;
+        String [] grafSign = new String[6];
+        int i = -1;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            while ((text = br.readLine()) != null) {
+                if(text.matches("[A-Z0-9]")){
+                    sign = text.charAt(0);
+                    i++;
+                }else if(i >= 0 && i <6){
+                    grafSign[i] = text;
+                    i++;
+                }
+                if(i == 6){
+                    alphabet.put(sign, grafSign);
+                    i = -1;
+                    grafSign = new String[6];
+                }
+            }
+        }catch(FileNotFoundException e){e.printStackTrace();}
+        catch(IOException e){e.printStackTrace();}
     }
 
     public void prepareListForPrint(){
@@ -60,4 +78,14 @@ public class ASCIIAlphabet {
             System.out.println();
         }
     }
+
+//    public void printListTest(){
+//        int quantityOfRows = alphabet.get('1').length;
+//        for(int numOfRows = 0; numOfRows < 6; numOfRows++){
+//            for(Map.Entry<Character, String[]> pair : alphabet.entrySet()){
+//                System.out.print(pair.getValue()[numOfRows]);
+//            }
+//            System.out.println();
+//        }
+//    }
 }
--- src/les10/hw/task4/Main.java
+++ src/les10/hw/task4/Main.java
@@ -1,11 +1,13 @@
 package les10.hw.task4;
 
+import java.io.File;
+
 /**
  * Created by User on 11.11.2016.
  */
 public class Main {
     public static void main(String[] args) {
-        ASCIIAlphabet asciiAlphabet = new ASCIIAlphabet();
+        ASCIIAlphabet asciiAlphabet = new ASCIIAlphabet(new File("c:/DevKit/Temp/Source/Alphabet.txt"));
         asciiAlphabet.prepareListForPrint();
         asciiAlphabet.printList();
     }
